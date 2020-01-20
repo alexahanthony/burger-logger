943dc8fe0993fbccf9e298f1f8c1e7424f3064f1
Alexita <alexahanthony@gmail.com>
2020-01-19 16:11:22 -0800

    dbtest2

--- config/connection.js
+++ config/connection.js
@@ -19,13 +19,15 @@ connection.connect(function(err) {
 });
 
 if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'hacktheplanet',
     database: 'todoagain_db'
-  })
-}
+  });
+};
 
 // Export connection for our ORM to use.
 module.exports = connection;
